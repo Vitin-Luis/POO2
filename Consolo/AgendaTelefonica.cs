9c4952f0aa398aff49f47f2ea692b35319b74f74
Victor Luis <101286309+Vitin-Luis@users.noreply.github.com>
2024-02-28 08:48:10 -0300

    consolo v0.2

---
+++ Consolo/AgendaTelefonica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolo
+{
+    public class AgendaTelefonica
+    {
+        List<Contato> contatos;
+
+        public AgendaTelefonica(List<Contato> contatos)
+        {
+            contatos = new List<Contato>();
+            this.contatos = contatos;
+        }
+
+        public void inserir(string nome, string numero)
+        {
+            contatos.Add(new Contato(nome, numero));
+        }
+        public string buscarNumero(string nome)
+        {
+            Contato contato;
+            
+        }
+        public void remover(string nome)
+        {
+            contatos.RemoveAt();
+        }
+        public int tamanho()
+        {
+            int cont = 0;
+            foreach (Contato contato in contatos)
+            {
+                cont++;
+            }
+            return cont;
+        }
+    }
+}
---
+++ Consolo/Contato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolo
+{
+    public class Contato
+    {
+        public string nome { get; set; }
+        public string numero { get; set; }
+
+        public Contato(string nome, string numero)
+        {
+            this.nome = nome;
+            this.numero = numero;
+        }
+    }
+}
